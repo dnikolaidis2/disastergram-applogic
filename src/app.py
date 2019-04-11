3c425fc71b128ad61c4ba5feead8dd7b38403604
Dimitris Nikolaidis <dnikolaidis4@gmail.com>
2019-04-11 23:32:57 +0300

    Baby steps

--- src/app.py
+++ src/app.py
@@ -1,12 +1,77 @@
-from flask import Flask
+from flask import Flask, request, jsonify, Response, abort
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
 
+# https://docs.sqlalchemy.org/en/latest/core/engines.html#postgresql
+# Instantiates database server.
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:1234@db/postgres'
+# TODO: WTF is this?
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!TEST'
+bcrypt = Bcrypt(app)
+
+db = SQLAlchemy(app)    # Creates an object of the database.
+ma = Marshmallow(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.Text, unique=True, nullable=False)
+
+    def check_password(self, bc, password):
+        return bc.check_password_hash(self.password, password)
+
+    def __init__(self, bc, **kwargs):
+        newkargs = kwargs
+        newkargs['password'] = bc.generate_password_hash(kwargs['password'].__str__())
+        super(User, self).__init__(**newkargs)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+# db.drop_all()
+db.create_all()   # Creates - Instantiates database tables.
+
+
+class UserSchema(ma.ModelSchema):
+    class Meta:
+        model = User
+
+
+@app.route('/auth/user', methods=['POST', 'GET'])
+def user():
+    if request.method == 'POST':
+        if not request.is_json:
+            abort(400)
+
+        newuser = User(bcrypt, username=request.json['username'], password=request.json['password'])
+        db.session.add(newuser)
+        db.session.commit()
+        return Response(status=201)
+
+    if request.method == 'GET':
+        user_schema = UserSchema()
+        getuser = User.query.get(3)
+        return user_schema.jsonify(getuser)
+        # Find if user exists
+        # request.args.get('username')
+
+        # Maybe check if they have the required perms?
+        # User
+        # Return user. Redirecting to its profile page
+        # return
+
+
+@app.route('/auth/login', methods=['POST'])
+def login():
+    pass
 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
+
