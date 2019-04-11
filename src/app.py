0139cabd7e238ecc325f58e1ffdc263d59a71e83
Dimitrris kastrinakis <dimitriskas.1997@gmail.com>
2019-04-12 01:22:05 +0300

    Added some GET debug functionalities
    * GET /auth/user now returns all usernames (as simple string)
    * GET /auth/user?username=userX now returns userX json obj
    * Ugly code is ugly. Fix immediately
    * Marshmallow now in (better) working condition

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify, Response, abort
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from marshmallow import pprint
 from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
@@ -27,16 +28,16 @@ class User(db.Model):
 
     def __init__(self, bc, **kwargs):
         newkargs = kwargs
-        newkargs['password'] = bc.generate_password_hash(kwargs['password'].__str__())
+        newkargs['password'] = bc.generate_password_hash(kwargs['password'].__str__()).decode('utf-8')
         super(User, self).__init__(**newkargs)
 
     def __repr__(self):
         return '<User %r>' % self.username
 
 
+
 # db.drop_all()
 db.create_all()   # Creates - Instantiates database tables.
-
 
 class UserSchema(ma.ModelSchema):
     class Meta:
@@ -46,7 +47,11 @@ class UserSchema(ma.ModelSchema):
 @app.route('/auth/user', methods=['POST', 'GET'])
 def user():
     if request.method == 'POST':
+        # TODO: could you use marshal.dump to do some input testing?
         if not request.is_json:
+            abort(400)
+
+        if User.query.filter(User.username == request.json['username']).count() != 0:
             abort(400)
 
         newuser = User(bcrypt, username=request.json['username'], password=request.json['password'])
@@ -55,11 +60,24 @@ def user():
         return Response(status=201)
 
     if request.method == 'GET':
-        user_schema = UserSchema()
-        getuser = User.query.get(3)
-        return user_schema.jsonify(getuser)
+        user_schema = UserSchema(exclude=['id'])
+        # getuser = User.query.get(1)
+
+        # return user_schema.jsonify(getuser)
         # Find if user exists
-        # request.args.get('username')
+
+        reqd_user = request.args.get('username')
+        if reqd_user is None:
+            # Returns all usernames in a single string
+            # ...for debugging reasons
+            str = ''
+            user_list = User.query.all()
+            for user in user_list:
+                str = str + user.username + '  '
+            return str
+        else:
+            get_user = User.query.filter(User.username == reqd_user).one()
+            return user_schema.jsonify(get_user)
 
         # Maybe check if they have the required perms?
         # User
@@ -69,7 +87,14 @@ def user():
 
 @app.route('/auth/login', methods=['POST'])
 def login():
-    pass
+    if request.method == 'POST':
+
+        if not request.is_json:
+            abort(400)
+
+        if User.query.filter(User.username == request.json['username']).count() != 0:
+            abort(400)
+
 
 
 if __name__ == '__main__':
